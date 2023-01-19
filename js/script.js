01877180fbaa3dd9900b8f089e915551f28c7aeb
Amira Lee <amiralee97@gmail.com>
2023-01-18 22:27:18 -0500

    Add images to middle section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function(){
+    const userRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await userRequest.json();
+};
+getData();
